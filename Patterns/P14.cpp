11cf6cdafedbb11a6551c43a250b0f87560128d1
Akanksha <akankshamali181@gmail.com>
2024-09-30 15:07:47 +0530

    s

---
+++ Patterns/P14.cpp
@@ -0,0 +1,20 @@
+// 1
+// 23
+// 345
+// 4567
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        int val=i;
+        for(int j=1;j<=i;j++){
+            cout<<val;
+            val++;
+        }
+        cout<<endl;
+    }
+}
---
+++ Patterns/P15.cpp
@@ -0,0 +1,21 @@
+// 1
+// 21
+// 321
+// 4321
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+        int val=i;
+        for(int j=1;j<=i;j++){
+            cout<<val;
+            val--;
+        }
+        cout<<endl;
+    }
+}
