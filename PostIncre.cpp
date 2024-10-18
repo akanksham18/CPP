0ff73cb8b1fcdafa608b7306431bff83ebf81310
Akanksha <akankshamali181@gmail.com>
2024-10-18 09:20:17 +0530

    m

---
+++ PostIncre.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 3;
+    int sum = 2+(a++);
+    cout<<sum<<endl;
+    cout<<a<<endl;
+}
--- RightShift.cpp
+++ RightShift.cpp
@@ -3,5 +3,5 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    cout<<(n>>2);
+    cout<<(n>>4)<<endl;
 }
