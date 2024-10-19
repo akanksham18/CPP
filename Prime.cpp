8fe2ec85e4bb624df877ed42fb419efbb139de88
Akanksha <akankshamali181@gmail.com>
2024-10-19 17:05:38 +0530

    a

--- Prime.cpp
+++ Prime.cpp
@@ -19,6 +19,7 @@ int main(){
     for(int i=2;i<=n-1;i++){
         if(i%2==0){
             isPrime=0;
+            break;
         }else{
             isPrime=1;
         }
---
+++ Variables.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<a<<endl;
+    for(int i=0;i<=0;i++){
+        int a=10;
+        cout<<a<<endl;
+    }
+
+    if(true){
+        int a=2;
+        cout<<a<<endl;
+        if(true){
+            int a=3;
+            cout<<a<<endl;
+        }
+    }
+}
