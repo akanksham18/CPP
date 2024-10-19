791440e5de294a71e94d85d7bf01675de0717f3f
Akanksha <akankshamali181@gmail.com>
2024-10-19 10:48:04 +0530

    a

---
+++ Prime.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter number"<<endl;
+    cin>>n;
+
+    bool isPrime=1;
+
+    if(n==1){
+        cout<<"Not Prime";
+    }
+
+    if(n==2){
+        cout<<"Prime";
+    }
+
+    for(int i=2;i<=n-1;i++){
+        if(i%2==0){
+            isPrime=0;
+        }else{
+            isPrime=1;
+        }
+    }
+
+    if(isPrime==1){
+        cout<<"Prime"<<endl;
+    }else{
+        cout<<"Not Prime"<<endl;
+    }
+}
